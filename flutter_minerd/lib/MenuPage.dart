9aff8b697f6386995492e6cf5212680dbd61747f
genri0303 <108440896+genri0303@users.noreply.github.com>
2024-08-05 17:23:11 -0400

    Another_Commit

---
+++ flutter_minerd/lib/MenuPage.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'RegisterVisitPage.dart';
+
+class MenuPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: 2,
+      padding: EdgeInsets.all(16.0),
+      children: <Widget>[
+        MenuButton(
+          icon: Icons.add,
+          label: 'Registrar Visita',
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => RegisterVisitPage()),
+            );
+          },
+        ),
+        
+
+        
+      ],
+    );
+  }
+}
+
+class MenuButton extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final VoidCallback onTap;
+
+  const MenuButton({required this.icon, required this.label, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        elevation: 2,
+        margin: EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(icon, size: 50, color: Colors.blue),
+            SizedBox(height: 10),
+            Text(
+              label,
+              style: TextStyle(fontSize: 16, color: Colors.blue),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
